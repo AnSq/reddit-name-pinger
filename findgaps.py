1fde8b25a3fce955f2ea8977423e5debec085c00
AnSq <ansq00@gmail.com>
2014-01-01 19:05:15 -0800

    findgaps now reports number of each type of file processed

--- findgaps.py
+++ findgaps.py
@@ -15,8 +15,12 @@ def main():
 	taken = []
 	failed = []
 	all = []
+	num_available = 0
+	num_taken = 0
+	num_failed = 0
 
 	for fname in os.listdir(path_available):
+		num_available += 1
 		path = os.path.join(path_available, fname)
 		print path
 		s = set()
@@ -32,6 +36,7 @@ def main():
 		write_list(available, file_out_available)
 
 	for fname in os.listdir(path_taken):
+		num_taken += 1
 		path = os.path.join(path_taken, fname)
 		print path
 		s = set()
@@ -47,6 +52,7 @@ def main():
 		write_list(taken, file_out_taken)
 
 	for fname in os.listdir(path_failed):
+		num_failed += 1
 		path = os.path.join(path_failed, fname)
 		print path
 		s = set()
@@ -61,19 +67,23 @@ def main():
 		failed.sort()
 		write_list(failed, file_out_failed)
 
+	num_all = num_available + num_taken + num_failed
 	all = available + taken + failed
 	all = list(set(all))
 	all.sort(key=convert.to_num)
 
 	print "\n===================\n"
-	print "Available: %d" % len(available)
-	print "Taken:     %d" % len(taken)
-	print "Failed:    %d" % len(failed)
-	print "Sum:       %d" % len(all)
+
+	print "Available: %d \tin %d files" % (len(available), num_available)
+	print "Taken:     %d \tin %d files" % (len(taken), num_taken)
+	print "Failed:    %d \tin %d files" % (len(failed), num_failed)
+	print "Sum:       %d \tin %d files" % (len(all), num_all)
 	print "Total:     %d" % pow(38,4)
 	print "Progress:  %.2f%%" % (100.0 * ((len(all) + 0.0) / pow(38,4)))
+
 	print "\n===================\n"
 
+	print "Gaps:"
 	gaps = open("gaps.txt", "w")
 	for i in range(0, len(all) - 1):
 		curr = convert.to_num(all[i])
